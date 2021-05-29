ad2ef092ea9286259c2163fbb0c76df350dcea5b
Wouter Buckens <wouter@epicteddy.com>
2021-05-29 17:51:29 +0200

    Improve tag name display on tag page for small breakpoints

--- pages/blog/tag/_tag.vue
+++ pages/blog/tag/_tag.vue
@@ -1,11 +1,11 @@
 <template>
 	<div>
-		<div class="flex justify-between my-4 -mb-2 px-6 py-4 bg-gray-100 rounded-sm">
-			<p class="flex-grow w-1/3">
+		<div class="sm:flex justify-between my-4 -mb-2 px-6 py-4 bg-gray-100 rounded-sm">
+			<p class="flex-grow sm:w-1/3">
 				Showing posts tagged
 				<strong>{{ tag }}</strong>
 			</p>
-			<p class="flex-none whitespace-nowrap">
+			<p class="sm:ml-4 flex-none whitespace-nowrap text-right">
 				<nuxt-link :to="{ name: 'blog' }"
 						   class="text-gray-500 underline hover:text-black focus:text-black">
 					Show all posts
@@ -17,17 +17,25 @@
 </template>
 
 <script lang="ts">
+import { Component, Vue } from 'nuxt-property-decorator';
 import { IContentDocument } from '@nuxt/content/types/content';
 import { Context } from '@nuxt/types';
-import { Component, Vue } from 'nuxt-property-decorator';
-import ContentList from '../../../components/ContentList.vue';
-import Post from '../../../models/Post';
+
+import Post from '~/models/Post';
+
+import ContentList from '~/components/ContentList.vue';
 
 @Component({
 	name: 'TagPage',
 	components: { ContentList },
 })
 export default class TagPage extends Vue {
+
+	head() {
+		return {
+			title: `Posts tagged ${ this.tag } - @woubuc`,
+		};
+	}
 
 	private posts: (IContentDocument & Post)[] = [];
 	private tag: string = '';
@@ -45,6 +53,5 @@ export default class TagPage extends Vue {
 
 		return { tag, posts };
 	}
-
 }
 </script>
