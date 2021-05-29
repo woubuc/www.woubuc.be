642bcd2810b74255a35b3741ff5338dca1033a5e
Wouter Buckens <wouter@epicteddy.com>
2021-05-29 18:33:19 +0200

    Add monetization meta tag

--- nuxt.config.js
+++ nuxt.config.js
@@ -9,6 +9,7 @@ export default {
 			{ charset: 'utf-8' },
 			{ name: 'viewport', content: 'width=device-width, initial-scale=1' },
 			{ name: 'description', content: 'Where I write about programming, tech and stuff.' },
+			{ name: 'monetization', content: '$ilp.uphold.com/9YjGeW4zW7pF' },
 		],
 		link: [
 			{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
